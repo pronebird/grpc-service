3b6982d7600b797ef658186c8d09a17d1311389b
Andrej Mihajlov <and.mikhaylov@gmail.com>
2024-03-30 11:03:32 +0100

    Wrap event loop into channels

--- src/main.rs
+++ src/main.rs
@@ -21,65 +21,62 @@ const LOG_DIRECTORY: &str = r"C:\Users\pronebird\Desktop";
 
 define_windows_service!(ffi_service_main, service_main);
 
+#[derive(Debug)]
+struct ServiceEvent {
+    service_control: ServiceControl,
+    completion_tx: oneshot::Sender<ServiceControlHandlerResult>,
+}
+
+impl ServiceEvent {
+    fn new(
+        service_control: ServiceControl,
+        completion_tx: oneshot::Sender<ServiceControlHandlerResult>,
+    ) -> Self {
+        Self {
+            service_control,
+            completion_tx,
+        }
+    }
+
+    fn service_control(&self) -> ServiceControl {
+        self.service_control
+    }
+
+    fn complete(self, result: ServiceControlHandlerResult) {
+        if self.completion_tx.send(result).is_err() {
+            tracing::error!("Failed to send a completion reply");
+        }
+    }
+}
+
 fn service_main(arguments: Vec<OsString>) {
     if let Err(e) = run_service(&arguments) {
         tracing::error!("error: {}", e);
     }
 }
 
-struct ShutdownSignal(oneshot::Sender<()>);
-impl ShutdownSignal {
-    fn new(sender: oneshot::Sender<()>) -> Self {
-        Self(sender)
-    }
-
-    fn acknowledge(self) {
-        if self.0.send(()).is_err() {
-            tracing::debug!("Failed to acknowledge shutdown.");
-        }
-    }
-}
-
 #[tracing::instrument(err)]
 fn run_service(arguments: &[OsString]) -> Result<(), Error> {
-    let address = SocketAddr::from_str("[::1]:10000").unwrap();
     let rt = tokio::runtime::Runtime::new().unwrap();
 
-    let (shutdown_tx, mut shutdown_rx) = mpsc::unbounded_channel();
+    let (event_tx, mut event_rx) = mpsc::unbounded_channel();
 
     let event_handler = move |control: ServiceControl| {
         tracing::debug!("received a control command: {:?}", control);
-        match control {
-            ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
-            ServiceControl::Stop => {
-                tracing::debug!("Sending shutdown...");
 
-                // Create a oneshot channel that will be used to initiate the shutdown and receive the acknowledgment upon completion.
-                let (completion_tx, completion_rx) = oneshot::channel();
+        let (completion_tx, completion_rx) = oneshot::channel();
 
-                // Send a shutdown signal.
-                match shutdown_tx.send(ShutdownSignal::new(completion_tx)) {
-                    Ok(()) => {
-                        tracing::debug!("Sent shutdown signal. Waiting for completion...");
-
-                        // Block current thread until the service has shut down or unless the channel errors.
-                        match completion_rx.blocking_recv() {
-                            Ok(()) => {
-                                tracing::debug!("Shutdown is complete. Return control to the service dispatcher.");
-                            }
-                            Err(e) => {
-                                tracing::debug!("Couldn't receive a shutdown completion: {}", e);
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        tracing::debug!("Couldn't send a shutdown signal: {}", e);
-                    }
-                }
-
-                ServiceControlHandlerResult::NoError
+        match event_tx.send(ServiceEvent::new(control, completion_tx)) {
+            Ok(()) => completion_rx
+                .blocking_recv()
+                .inspect_err(|e| {
+                    tracing::error!("Couldn't receive a completion reply: {}", e);
+                })
+                .unwrap_or(ServiceControlHandlerResult::Other(127)),
+            Err(e) => {
+                tracing::error!("Couldn't send the event: {}", e);
+                ServiceControlHandlerResult::Other(128)
             }
-            _ => ServiceControlHandlerResult::NotImplemented,
         }
     };
 
@@ -98,31 +95,31 @@ fn run_service(arguments: &[OsString]) -> Result<(), Error> {
     tracing::info!("service status is 'running'");
 
     rt.block_on(async {
-        // Create cancellation token that will be used to shut down the service.
         let shutdown_token = CancellationToken::new();
-
-        // Clone shutdown token and convert it into owned cancellation future,
-        // which will complete when shutdown_token is cancelled.
         let shutdown_fut = shutdown_token.clone().cancelled_owned();
-        let service_join_handle = tokio::spawn(service::run(address, shutdown_fut));
 
-        // Wait for shutdown message.
-        let shutdown = shutdown_rx.recv().await;
+        let address = SocketAddr::from_str("[::1]:10000").unwrap();
+        let mut service_join_handle = Some(tokio::spawn(service::run(address, shutdown_fut)));
 
-        // Cancel shutdown token to resolve the shutdown_fut.
-        tracing::debug!("Shutdown triggered. Cancelling the service...");
-        shutdown_token.cancel();
-        if let Err(e) = service_join_handle.await {
-            tracing::error!("Couldn't join on service: {}", e);
-        }
+        while let Some(service_event) = event_rx.recv().await {
+            let service_control_result: ServiceControlHandlerResult =
+                match service_event.service_control() {
+                    ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
+                    ServiceControl::Stop => {
+                        shutdown_token.cancel();
 
-        if let Some(shutdown) = shutdown {
-            tracing::debug!(
-                "The service has shut down. Send the reply back to control event handler."
-            );
-            shutdown.acknowledge();
-        } else {
-            tracing::debug!("The shutdown channel was dropped too soon!");
+                        if let Some(service_join_handle) = service_join_handle.take() {
+                            if let Err(e) = service_join_handle.await {
+                                tracing::error!("Couldn't join on service: {}", e);
+                            }
+                        }
+
+                        ServiceControlHandlerResult::NoError
+                    }
+                    _ => ServiceControlHandlerResult::NotImplemented,
+                };
+
+            service_event.complete(service_control_result);
         }
     });
 
